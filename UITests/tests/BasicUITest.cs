771dfe2761c6b50af64d21f8734673136e57c3a7
ridomin <ridomin@live.com>
2019-04-18 17:07:31 -0700

    Add UITests

---
+++ UITests/tests/BasicUITest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tests
+{
+    [TestClass]
+    public class BasicUITest : Session
+    {
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Setup(context);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            TearDown();
+        }
+
+        [TestInitialize]
+        public void Clear()
+        {
+
+        }
+
+        [TestMethod]
+        public void NavigateMainTabs()
+        {
+            Console.WriteLine("Trying to Find Home Button");
+            session.FindElementByName("ButtonShowRuntimeVersionInfo").Click();
+        }
+    }
+}
